85bf5515cd3001a1991437e33d6bd840026d2807
Johan Lautakoski <johan.lautakoski@eyevinn.se>
2023-02-27 11:21:44 +0100

    added support for demuxed audio (#19)
    * added support for demuxed audio
    
    * changed version of m3u8 package and added test
    
    * added test for cmaf and fixed a bug
    
    * new version of m3u8 lib and fixed test
    
    * fixed pr comments
    
    ---------
    
    Co-authored-by: Jonas Birmé <jonas.birme@eyevinn.se>

--- index.js
+++ index.js
@@ -8,44 +8,105 @@ class HLSRepeatVod {
     this.playlists = {};
     this.repetitions = repetitions;
     this.bandwiths = [];
+    this.audioSegments = {};
   }
 
-  load(_injectMasterManifest, _injectMediaManifest) {
+  load(_injectMasterManifest, _injectMediaManifest, _injectAudioManifest) {
     return new Promise((resolve, reject) => {
       const parser = m3u8.createStream();
 
       parser.on('m3u', m3u => {
         this.m3u = m3u;
         let mediaManifestPromises = [];
+        let audioManifestPromises = [];
+        let audioGroups = {};
         let baseUrl;
         const m = this.masterManifestUri.match(/^(.*)\/.*?$/);
         if (m) {
           baseUrl = m[1] + '/';
         }
+
+
         for (let i = 0; i < m3u.items.StreamItem.length; i++) {
           const streamItem = m3u.items.StreamItem[i];
           this.bandwiths.push(streamItem.get('bandwidth'));
 
           const mediaManifestUrl = url.resolve(baseUrl, streamItem.get('uri'));
-          mediaManifestPromises.push(this._loadMediaManifest(mediaManifestUrl, streamItem.get('bandwidth'), _injectMediaManifest));
+          if (!m3u.items.MediaItem.find((mediaItem) => mediaItem.get("type") === "AUDIO" && mediaItem.get("uri") == streamItem.get("uri"))) {
+            mediaManifestPromises.push(this._loadMediaManifest(mediaManifestUrl, streamItem.get("bandwidth"), _injectMediaManifest));
+          }
+
+          if (streamItem.attributes.attributes["audio"]) {
+            let audioGroupId = streamItem.attributes.attributes["audio"];
+            if (!this.audioSegments[audioGroupId]) {
+              this.audioSegments[audioGroupId] = {};
+            }
+
+            let audioGroupItems = m3u.items.MediaItem.filter((item) => {
+              return item.attributes.attributes.type === "AUDIO" && item.attributes.attributes["group-id"] === audioGroupId;
+            });
+            // # Find all langs amongst the mediaItems that have this group id.
+            // # It extracts each mediaItems language attribute value.
+            // # ALSO initialize in this.audioSegments a lang. property whos value is an array [{seg1}, {seg2}, ...].
+            let audioLanguages = audioGroupItems.map((item) => {
+              let itemLang;
+              if (!item.attributes.attributes["language"]) {
+                itemLang = item.attributes.attributes["name"];
+              } else {
+                itemLang = item.attributes.attributes["language"];
+              }
+              // Initialize lang. in new group.
+              if (!this.audioSegments[audioGroupId][itemLang]) {
+                this.audioSegments[audioGroupId][itemLang] = [];
+              }
+              return (item = itemLang);
+            });
+
+            // # For each lang, find the lang playlist uri and do _loadAudioManifest() on it.
+            for (let j = 0; j < audioLanguages.length; j++) {
+              let audioLang = audioLanguages[j];
+              let audioUri = audioGroupItems[j].attributes.attributes.uri
+              if (!audioUri) {
+                //# if mediaItems dont have uris
+                let audioVariant = m3u.items.StreamItem.find((item) => {
+                  return !item.attributes.attributes.resolution && item.attributes.attributes["audio"] === audioGroupId;
+                });
+                if (audioVariant) {
+                  audioUri = audioVariant.properties.uri;
+                }
+              }
+              if (audioUri) {
+                let audioManifestUrl = url.resolve(baseUrl, audioUri);
+                if (!audioGroups[audioGroupId]) {
+                  audioGroups[audioGroupId] = {};
+                }
+                // # Prevents 'loading' an audio track with same GroupID and LANG.
+                // # otherwise it just would've loaded OVER the latest occurrent of the LANG in GroupID.
+                if (!audioGroups[audioGroupId][audioLang]) {
+                  audioGroups[audioGroupId][audioLang] = true;
+                  audioManifestPromises.push(this._loadAudioManifest(audioManifestUrl, audioGroupId, audioLang, _injectAudioManifest));
+                }
+              }
+            }
+          }
         }
-        Promise.all(mediaManifestPromises)
-        .then(resolve)
-        .catch(reject);
+        Promise.all(mediaManifestPromises.concat(audioManifestPromises))
+          .then(resolve)
+          .catch(reject);
       });
       parser.on('error', (err) => {
         reject("Failed to parse M3U8: " + err);
       });
       if (!_injectMasterManifest) {
         fetch(this.masterManifestUri)
-        .then(res => {
-          res.body.pipe(parser);
-        })
-        .catch(reject);
+          .then(res => {
+            res.body.pipe(parser);
+          })
+          .catch(reject);
       } else {
         _injectMasterManifest().pipe(parser);
       }
-    });  
+    });
   }
 
   getBandwidths() {
@@ -54,6 +115,22 @@ class HLSRepeatVod {
 
   getMediaManifest(bw) {
     return this.playlists[bw].toString();
+  }
+
+  getAudioManifest(audioGroupId, lang) {
+    if (!this.audioSegments[audioGroupId]) {
+      const keygroup = Object.keys(this.audioSegments)
+      const audioSegementsGroup = this.audioSegments[keygroup[0]]
+      if (!audioSegementsGroup[lang]) {
+        const keylang = Object.keys(audioSegementsGroup)
+        return audioSegementsGroup[keylang[0]].toString();
+      }
+      return audioSegementsGroup[lang].toString();
+    } else if (!this.audioSegments[audioGroupId][lang]) {
+      const keylang = Object.keys(this.audioSegments[audioGroupId])
+      return this.audioSegments[audioGroupId][keylang[0]].toString();
+    }
+    return this.audioSegments[audioGroupId][lang].toString();
   }
 
   _loadMediaManifest(mediaManifestUri, bandwidth, _injectMediaManifest) {
@@ -79,12 +156,45 @@ class HLSRepeatVod {
       });
       if (!_injectMediaManifest) {
         fetch(mediaManifestUri)
-        .then(res => {
-          res.body.pipe(parser);
-        })
-        .catch(reject);
+          .then(res => {
+            res.body.pipe(parser);
+          })
+          .catch(reject);
       } else {
         _injectMediaManifest(bandwidth).pipe(parser);
+      }
+    });
+  }
+
+  _loadAudioManifest(audioManifestUri, audioGroupId, audioLang, _injectAudioManifest) {
+    return new Promise((resolve, reject) => {
+      const parser = m3u8.createStream();
+
+      parser.on('m3u', m3u => {
+        if (!this.audioSegments[audioGroupId][audioLang].length) {
+          this.audioSegments[audioGroupId][audioLang] = m3u;
+        }
+
+        let playlistItems = this.audioSegments[audioGroupId][audioLang].items.PlaylistItem;
+        playlistItems[0].set('discontinuity', true);
+        let repetition = 0;
+        while (repetition < this.repetitions - 1) {
+          this.audioSegments[audioGroupId][audioLang].items.PlaylistItem = this.audioSegments[audioGroupId][audioLang].items.PlaylistItem.concat(playlistItems);
+          repetition++;
+        }
+        resolve();
+      });
+      parser.on('error', (err) => {
+        reject("Failed to parse M3U8: " + err);
+      });
+      if (!_injectAudioManifest) {
+        fetch(audioManifestUri)
+          .then(res => {
+            res.body.pipe(parser);
+          })
+          .catch(reject);
+      } else {
+        _injectAudioManifest(audioGroupId, audioLang).pipe(parser);
       }
     });
   }
--- spec/hls_repeat_spec.js
+++ spec/hls_repeat_spec.js
@@ -44,6 +44,82 @@ describe("HLSRepeatVod", () => {
     });
   });
 
+  it("can create a audio manifest", done => {
+    const mockVod = new HLSRepeatVod('http://mock.com/mock.m3u8', 3, {});
+
+    // An example on how it can be initiated from a string instead of URL
+    const masterManifest = fs.readFileSync('testvectors/hls_demuxed/master.m3u8', 'utf8');
+    const mediaManifest = fs.readFileSync('testvectors/hls_demuxed/8850073.m3u8', 'utf8');
+    const audioManifest = fs.readFileSync('testvectors/hls_demuxed/aac-en.m3u8', 'utf8');
+    let masterManifestStream = new Readable();
+    masterManifestStream.push(masterManifest);
+    masterManifestStream.push(null);
+
+    let mediaManifestStream = new Readable();
+    mediaManifestStream.push(mediaManifest);
+    mediaManifestStream.push(null);
+
+    let audioManifestStream = new Readable();
+    audioManifestStream.push(audioManifest);
+    audioManifestStream.push(null);
+
+    mockVod.load(() => { return masterManifestStream }, () => { return mediaManifestStream }, () => { return audioManifestStream })
+    .then(() => {
+        const manifest = mockVod.getAudioManifest("aac","en");
+        const lines = manifest.split("\n");
+        expect(lines[76].match(/seg_en_\d+.ts/)).not.toBeNull();
+        expect(lines[77]).toEqual('#EXT-X-DISCONTINUITY');
+        expect(lines[78].match("#EXT-X-CUE-OUT:DURATION=15.120000000000001")).not.toBeNull();
+        expect(lines[84].match("#EXT-X-CUE-IN")).not.toBeNull();
+        expect(lines[147].match(/seg_en_\d+.ts/)).not.toBeNull();
+        expect(lines[148]).toEqual('#EXT-X-DISCONTINUITY');
+        expect(lines[149].match("#EXT-X-CUE-OUT:DURATION=15.120000000000001")).not.toBeNull();
+        expect(lines[155].match("#EXT-X-CUE-IN")).not.toBeNull();
+        expect(lines[218].match(/seg_en_\d+.ts/)).not.toBeNull();
+      done();
+    });
+  });
+
+
+  it("can repeat a cmaf audio manifest", done => {
+    const mockVod = new HLSRepeatVod('http://mock.com/mock.m3u8', 3, {});
+
+    // An example on how it can be initiated from a string instead of URL
+    const masterManifest = fs.readFileSync('testvectors/hls_demuxed_cmaf/master.m3u8', 'utf8');
+    const mediaManifest = (bw) => { 
+      const bwmap = {
+        454000: "0",
+        984000: "1",
+        1726000: "2",
+        2786000: "3",
+        3846000: "4",
+      }
+      return fs.createReadStream(`testvectors/hls_demuxed_cmaf/level_${bwmap[bw]}.m3u8`);
+    };
+    const audioManifest = fs.readFileSync('testvectors/hls_demuxed_cmaf/audio.m3u8');
+    let masterManifestStream = new Readable();
+    masterManifestStream.push(masterManifest);
+    masterManifestStream.push(null);
+
+    let audioManifestStream = new Readable();
+    audioManifestStream.push(audioManifest);
+    audioManifestStream.push(null);
+
+    mockVod.load(() => { return masterManifestStream }, mediaManifest, () => { return audioManifestStream })
+    .then(() => {
+        const manifest = mockVod.getAudioManifest("audio-aacl-128","Svenska");
+        const lines = manifest.split("\n");
+        expect(lines[27].match("hls/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478-audio=128000-104")).not.toBeNull();
+        expect(lines[28].match(`#EXT-X-MAP:URI="https://vod.streaming.a2d.tv/a07ff4eb-6770-4805-a0ad-a4d1b127880d/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478.ism/hls/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478-audio=128000.m4s"`)).not.toBeNull();
+        expect(lines[29]).toEqual('#EXT-X-DISCONTINUITY');
+        expect(lines[47].match("hls/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478-audio=128000-104")).not.toBeNull();
+        expect(lines[28].match(`#EXT-X-MAP:URI="https://vod.streaming.a2d.tv/a07ff4eb-6770-4805-a0ad-a4d1b127880d/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478.ism/hls/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478-audio=128000.m4s"`)).not.toBeNull();
+        expect(lines[49]).toEqual('#EXT-X-DISCONTINUITY');
+        expect(lines[67].match("hls/4fef8b00-6d0b-11ed-89b6-2b1a288899a0_20356478-audio=128000-104")).not.toBeNull();
+      done();
+    });
+  });
+
   it("can generate HLS from a downloaded HLS", done => {
     const hlsVod = new HLSRepeatVod('http://testcontent.eyevinn.technology/slates/ottera/playlist.m3u8', 3);
     hlsVod.load()
